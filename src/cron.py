b9ef340ecb9b96cfa2c65f60477d55270dbd9f36
ginop-1 <enrico.covili@fermi.mo.it>
2022-01-03 22:36:53 +0100

    no more flood errors

--- src/cron.py
+++ src/cron.py
@@ -1,4 +1,5 @@
 from telethon import TelegramClient
+from telethon.errors import FloodWaitError
 from utils import Utils
 import asyncio
 import re
@@ -8,9 +9,16 @@ async def journal_log():
     bot = TelegramClient("cron", api_id=Utils.APP_ID, api_hash=Utils.APP_HASH)
     await bot.start(bot_token=Utils.TOKEN)
     await bot.connect()
-    gino = await bot.get_entity("https://t.me/ilginop")
+    gino_id = Utils.WHITELIST_IDS[0]
+    try:
+        # using ID instead of nick is needed to access
+        # cache instead of making continous requests
+        # that bring to flood
+        gino = await bot.get_entity(gino_id)
+    except FloodWaitError as e:
+        return print(f"Too may request. {e}")
     cmd = Utils.status_commands[2].get("🌡️ temp")
-    temp = await Utils._exec(gino.id, cmd=cmd, name="🌡️ temp")
+    temp = await Utils._exec(gino_id, cmd=cmd, name="🌡️ temp")
     temp = Utils.get_temperature(temp)
     temp = re.search(r"[0-9]+\.[0-9]+", temp).group(0)
     if float(temp) >= 50.00:

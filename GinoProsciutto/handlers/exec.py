9b214d20362cb460faf7ff8dafb969c143de4c58
ginop-1 <enrico.covili@fermi.mo.it>
2024-02-25 13:51:50 +0100

    fixed small issue with exec logging

--- GinoProsciutto/handlers/exec.py
+++ GinoProsciutto/handlers/exec.py
@@ -6,6 +6,6 @@ import logging
 @events.register(events.NewMessage(pattern="/exec"))
 async def exec(event: events.newmessage.NewMessage.Event):
     msg = event.text.split()[1:]
-    logging.info("received: exec " + msg)
+    logging.info("received: exec " + " ".join(msg))
     out = await Utils._exec(event.chat_id, cmd=msg)
     await event.reply(out[:4000])

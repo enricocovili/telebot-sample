5f26ada2e6232fe51e099168a85647add86ff030
enricocovili <henrycovili2004@gmail.com>
2026-02-20 12:28:17 +0100

    small fix for permission in minecraft command

--- GinoProsciutto/handlers/minecraft_server.py
+++ GinoProsciutto/handlers/minecraft_server.py
@@ -11,33 +11,39 @@ async def start_minecraft_server(event: events.newmessage.NewMessage):
     if event.chat_id not in Utils.MINECRAFT_WHITELIST_IDS:
         await event.reply("❌ You are not allowed to use this command ❌")
         return
+    
+    # i need permissions to execute the command. i'm copying the first id in the whitelist 
+    # and using it to execute the command
+
+    fake_privileged_chat_id = Utils.WHITELIST_IDS[0]
 
     bot = event.client
-    msg = await bot.send_message(event.chat_id, "⏳ Starting Minecraft server...")
+    msg = await bot.send_message(fake_privileged_chat_id, "⏳ Starting Minecraft server...")
 
     mac = os.getenv("MINECRAFT_SERVER_MAC")
     host = os.getenv("MINECRAFT_SERVER_HOST")
     user = os.getenv("MINECRAFT_SERVER_USERNAME")
     password = os.getenv("MINECRAFT_SERVER_PASSWORD")
 
-    await Utils._exec(event.chat_id, f"wakeonlan {mac}")
+    wake_on_lan_output = await Utils._exec(fake_privileged_chat_id, ["wakeonlan", mac])
+    logging.debug(f"Wake-on-LAN output: {wake_on_lan_output}")
     await msg.edit("⏳ Wake-on-LAN sent, waiting for host to come online...")
 
     while True:
-        result = await Utils._exec(event.chat_id, ["ping", "-c", "1", host])
+        result = await Utils._exec(fake_privileged_chat_id, ["ping", "-c", "1", host])
         if "1 received" in result:
             break
         await asyncio.sleep(5)
 
     await msg.edit("✅ Host is online! Starting server... ⏳")
     start_command = await Utils._exec(
-        event.chat_id,
+        fake_privileged_chat_id,
         f"sshpass -p {password} ssh -o StrictHostKeyChecking=no -l {user} {host}".split(
             " "
         )
         + ["'D: && cd Servers\\S_Mine && .\\mcserver.exe start'"],
     )
-    logging.info(f"Start command output: {start_command}")
+    logging.debug(f"Start command output: {start_command}")
     if "has already started" in start_command:
         await msg.edit("✅ Minecraft server is already running!")
         return
@@ -50,22 +56,27 @@ async def stop_minecraft_server(event: events.newmessage.NewMessage):
     if event.chat_id not in Utils.MINECRAFT_WHITELIST_IDS:
         await event.reply("❌ You are not allowed to use this command ❌")
         return
+    
+    # i need permissions to execute the command. i'm copying the first id in the whitelist 
+    # and using it to execute the command
+
+    fake_privileged_chat_id = Utils.WHITELIST_IDS[0]
 
     bot = event.client
-    msg = await bot.send_message(event.chat_id, "⏳ Stopping Minecraft server...")
+    msg = await bot.send_message(fake_privileged_chat_id, "⏳ Stopping Minecraft server...")
 
     host = os.getenv("MINECRAFT_SERVER_HOST")
     user = os.getenv("MINECRAFT_SERVER_USERNAME")
     password = os.getenv("MINECRAFT_SERVER_PASSWORD")
 
     output = await Utils._exec(
-        event.chat_id,
+        fake_privileged_chat_id,
         f"sshpass -p {password} ssh -o StrictHostKeyChecking=no -l {user} {host}".split(
             " "
         )
         + [f"'D: && cd Servers\\S_Mine && .\\mccron\mcrcon.exe -p {password} stop'"],
     )
-    logging.info(f"Stop command output: {output}")
+    logging.debug(f"Stop command output: {output}")
     if "Connection failed" in output:
         await msg.edit("✅ Minecraft server is already stopped!")
         return
